126be1ea8648c254cdd0765ff3fbcf7e5d6b552b
J N Sudhir <40768697+jnsudhir@users.noreply.github.com>
2020-06-25 19:28:59 +0530

    Update Linear search.py

--- Linear search.py
+++ Linear search.py
@@ -1,13 +1,16 @@
-def search(arr,n, x):
+#taking input from the user
+def ls(arr,n,x):
     for i in range(n):
-        if arr[i] == x:
+        if arr[i]==x:
             return i
     return -1
-arr = [12,23,34,45,56,67,78,89]
-x = 56
+arr = list(map(int,input("enter array list:").split()))
+x = int(input("enter value to search:"))
 n = len(arr)
-result = search(arr,n, x)
-if(result == -1):
-    print("arr is not listed")
+result = ls(arr,n,x)
+if(result==-1):
+    print("element is not in the list")
 else:
-    print("ele is at index:", result)
+    print("elemant is at index", result)
+  
+
